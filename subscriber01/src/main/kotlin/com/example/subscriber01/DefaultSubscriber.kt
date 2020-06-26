abc0d11525b8245f620c908653c0e536880d0b76
김정근 <gogoogjg@tripstore.kr>
2020-06-26 17:32:53 +0900

    pub-sub 방식의 메시지 송수신을 테스트한다

--- subscriber01/src/main/kotlin/com/example/subscriber01/DefaultSubscriber.kt
+++ subscriber01/src/main/kotlin/com/example/subscriber01/DefaultSubscriber.kt
@@ -7,8 +7,14 @@ import org.springframework.stereotype.Component
 class DefaultSubscriber {
 
     @JmsListener(destination = "temp")
-    fun subscribe(message: String) {
-        println("@@@@ subscribe @@@")
+    fun subscribe01(message: String) {
+        println("@@@@ subscribe01 @@@")
+        println(message)
+    }
+
+    @JmsListener(destination = "temp")
+    fun subscribe02(message: String) {
+        println("@@@@ subscribe02 @@@")
         println(message)
     }
 }
