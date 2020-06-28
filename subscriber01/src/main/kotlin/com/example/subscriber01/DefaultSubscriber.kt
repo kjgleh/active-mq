5517eb7c68301f179db02656fff83188f2f93bba
김정근 <gogoogjg@tripstore.kr>
2020-06-28 11:15:23 +0900

    subscriber01의 destination clientId값을 변경한다
    subscriber02 application을 추가하고 각각 clientId를 가지기 위해
    subscriber01의 destination clientId값을 subscriber01로 변경한다
    
    clientId값이 같으면 동일한 queue이므로 동일한 정책이 적용된다
    application별로 deadLetter를 쌓기 위해 다른 queue정책이 필요하다

--- subscriber01/src/main/kotlin/com/example/subscriber01/DefaultSubscriber.kt
+++ subscriber01/src/main/kotlin/com/example/subscriber01/DefaultSubscriber.kt
@@ -6,15 +6,15 @@ import org.springframework.stereotype.Component
 @Component
 class DefaultSubscriber {
 
-    @JmsListener(destination = "Consumer.kjg.VirtualTopic.temp")
+    @JmsListener(destination = "Consumer.subscriber01.VirtualTopic.temp")
     fun subscribe01(message: String) {
-        println("@@@@ subscribe01 @@@")
+        println("@@@@ subscribe01-01 @@@")
         println(message)
     }
 
-    @JmsListener(destination = "Consumer.kjg.VirtualTopic.temp")
+    @JmsListener(destination = "Consumer.subscriber01.VirtualTopic.temp")
     fun subscribe02(message: String) {
-        println("@@@@ subscribe02 @@@")
+        println("@@@@ subscribe01-02 @@@")
         println(message)
     }
 }
